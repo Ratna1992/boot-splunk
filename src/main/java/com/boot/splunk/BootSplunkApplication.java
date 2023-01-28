4f9eb853685bcd09ecb3878d42741076706d171a
ratna1992 <ratnasrinivas1992@gmail.com>
2023-01-28 15:36:13 +0530

    added new module

---
+++ src/main/java/com/boot/splunk/BootSplunkApplication.java
@@ -0,0 +1,13 @@
+package com.boot.splunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSplunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSplunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/splunk/BootSplunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.splunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSplunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
